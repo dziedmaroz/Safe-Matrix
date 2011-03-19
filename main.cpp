14be14077a67bb3f35c0322bd68ecbec5f8751a4
DziedMaroz <ilya.arkhanhelsky@gmail.com>
2011-03-19 07:08:20 +0200

    + toTriangleMatrix (need to finish)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <stdio.h>
-
-int main(int argc, char *argv[])
+#include "matrix.h"
+int main(int argc, char *argv[])        
 {
+   FILE* fin = fopen ("matrix.in","r");
+   int n,m;
+   fscanf(fin,"%d %d",&n,&m);
+   Matrix matr (n,m);
+   for (int i=0;i<n;i++)
+       for (int j=0;j<m;j++)
+         fscanf (fin,"%lf",&matr[i][j]);
+
+  // matr.makeUnitMatrix (n);
+   matr+=matr;
+
+   for (int i=0;i<n;i++)
+   {
+       for (int j=0;j<m;j++)
+         printf ("%9.2lf ",matr[i][j]);
+       printf ("\n");
+
+   }
+   printf ("\n\n");
+   Matrix matrTr = matr.toTriangleMatrix ();
+   for (int i=0;i<n;i++)
+   {
+       for (int j=0;j<m;j++)
+         printf ("%9.2lf ",matrTr[i][j]);
+       printf ("\n");
+
+   }
+
+   fclose (fin);
    return 0;
 }
